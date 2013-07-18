f297cd06437542c0cbfd94fd717ab88b0ec35e54
Bill Bryant <bill@wjbryant.com>
2013-07-18 10:59:47 -0400

    use hook system for jQuery adapter, fix whitespace

--- src/jquery.taboverride.style.js
+++ src/jquery.taboverride.style.js
@@ -38,23 +38,21 @@ Copyright (c) 2013 Bill Bryant | http://opensource.org/licenses/mit */
 	"use strict";
 
 	var $fnTabOverride = $.fn.tabOverride,
-		addDelegatedListeners = $fnTabOverride.utils.addDelegatedListeners,
-		removeDelegatedListeners = $fnTabOverride.utils.removeDelegatedListeners,
 		cache = [];
 
 	// for delegated events, add and remove the classes on the container element
 	function addStyles( $container, selector ) {
 		var utils = $fnTabOverride.style.utils;
-		utils.addEnabledClass( $container[0] );
-		utils.addActiveClass( $container[0] );
-		utils.addTabSizeCSSSelector( '.(enabledClass) ' + selector );
+		utils.addEnabledClass( $container[ 0 ] );
+		utils.addActiveClass( $container[ 0 ] );
+		utils.addTabSizeCSSSelector( ".(enabledClass) " + selector );
 	}
 
 	function removeStyles( $container, selector ) {
 		var utils = $fnTabOverride.style.utils;
-		utils.removeEnabledClass( $container[0] );
-		utils.removeActiveClass( $container[0] );
-		utils.removeTabSizeCSSSelector( '.(enabledClass) ' + selector );
+		utils.removeEnabledClass( $container[ 0 ] );
+		utils.removeActiveClass( $container[ 0 ] );
+		utils.removeTabSizeCSSSelector( ".(enabledClass) " + selector );
 	}
 
 	function addCacheItem( $container, selector ) {
@@ -66,8 +64,8 @@ Copyright (c) 2013 Bill Bryant | http://opensource.org/licenses/mit */
 			len = cache.length;
 
 		for ( i = 0; i < len; i += 1 ) {
-			if ( cache[i].$container === $container &&
-					cache[i].selector === selector ) {
+			if ( cache[ i ].$container === $container &&
+					cache[ i ].selector === selector ) {
 				cache.splice( i, 1 );
 				break;
 			}
@@ -76,35 +74,31 @@ Copyright (c) 2013 Bill Bryant | http://opensource.org/licenses/mit */
 
 	$fnTabOverride.style = tabOverride.style;
 
-	$fnTabOverride.addDelegatedExtension(function ( $container, selector, enable ) {
-		(enable ? addCacheItem : removeCacheItem)( $container, selector );
+	$fnTabOverride.addExtension( "setDelegated", function ( $container, selector, enable ) {
+		( enable ? addCacheItem : removeCacheItem )( $container, selector );
 
 		if ( $fnTabOverride.style() ) {
-			(enable ? addStyles : removeStyles)( $container, selector );
+			( enable ? addStyles : removeStyles )( $container, selector );
 		}
 	});
 
 	// compatibility with other extensions that add and remove the listeners directly
-	$fnTabOverride.utils.addDelegatedListeners = function ( $container, selector ) {
+	$fnTabOverride.addExtension( "addDelegatedListeners", function ( $container, selector ) {
 		if ( $fnTabOverride.style() ) {
-			$fnTabOverride.style.utils.addActiveClass( $container[0] );
+			$fnTabOverride.style.utils.addActiveClass( $container[ 0 ] );
 		}
-		addDelegatedListeners( $container, selector );
-	};
-	$fnTabOverride.utils.removeDelegatedListeners = function ( $container, selector ) {
+	});
+	$fnTabOverride.addExtension( "removeDelegatedListeners", function ( $container, selector ) {
 		if ( $fnTabOverride.style() ) {
-			$fnTabOverride.style.utils.removeActiveClass( $container[0] );
+			$fnTabOverride.style.utils.removeActiveClass( $container[ 0 ] );
 		}
-		removeDelegatedListeners( $container, selector );
-	};
+	});
 
 	$fnTabOverride.style.addExtension(function ( enable ) {
-		var i,
-			len = cache.length,
-			editStyles = enable ? addStyles : removeStyles;
+		var editStyles = enable ? addStyles : removeStyles;
 
-		for ( i = 0; i < len; i += 1 ) {
-			editStyles( cache[i].$container, cache[i].selector );
-		}
+		$.each( cache, function () {
+			editStyles( this.$container, this.selector );
+		});
 	});
 }));
